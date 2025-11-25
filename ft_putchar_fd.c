2a96b3a16865ad53ff1f86bb9942d74974b7cce7
Flunkie Deva Station <flunkademic@gmail.com>
2025-11-25 12:36:28 +0000

    Create ft_putchar_fd.c

---
+++ ft_putchar_fd.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*
+int	main(void)
+{
+	ft_putchar_fd('a', 2);
+	ft_putchar_fd('a', 0);
+	ft_putchar_fd('a', 1);
+	return (0);
+}
+*/
