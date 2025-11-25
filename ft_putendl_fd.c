f2b14756d2113a4f1115f25a83bb835f397fe1ef
Flunkie Deva Station <flunkademic@gmail.com>
2025-11-25 12:38:21 +0000

    Create ft_putendl_fd.c

---
+++ ft_putendl_fd.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+	ft_putchar_fd('\n', fd);
+}
+/*
+int	main(void)
+{
+	ft_putendl_fd("Aloha!", 1);
+}
+*/
